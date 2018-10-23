fe8c10088ba3b126aa814bc0ffff73d51e1bc383
Sergio Trujillo del Valle <trujillo.sergio6@gmail.com>
2018-10-22 22:15:21 -0500

    configuracion postgres heroku

--- config/database.js
+++ config/database.js
@@ -78,6 +78,7 @@ module.exports = {
       password: Env.get('DB_PASSWORD', 'DATABASE_URL.password'),
       database: Env.get('DB_DATABASE', 'DATABASE_URL.pathname.substr(1)')
     },
+    
     debug: Env.get('DB_DEBUG', false)
   }
 }
